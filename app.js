d0460e11b14a48051eb286b5dffcac459a12130e
divyabilson <137600081+divyabilson@users.noreply.github.com>
2023-07-09 23:37:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express')
 const app = express()
 
-app.get ('/', (req, res) => res.send('Hello Devops Team'))
+app.get ('/', (req, res) => res.send('Hello New Devops Team'))
 app.listen(3000, () => console.log('Server ready'))
