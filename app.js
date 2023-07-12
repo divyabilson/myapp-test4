9038d3d2481849c2747c50c633d18012f3d69ed2
divyabilson <137600081+divyabilson@users.noreply.github.com>
2023-07-13 00:26:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require ('express')
 const app = express()
 
-app.get ('/', (req, res) => res.send('Hello! from staging environment!!!!!!!'))
+app.get ('/', (req, res) => res.send('Hello! from staging'))
 app.listen(3000, () => console.log('Server ready'))
